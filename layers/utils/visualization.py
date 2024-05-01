d6fabbb2c9e49a744e2c54f500be6e95030c065f
Kai Lan <kai.weixian.lan@gmail.com>
2024-05-01 01:14:44 -0700

    fixed aspect ratio for human mesh viewer

--- layers/utils/visualization.py
+++ layers/utils/visualization.py
@@ -15,12 +15,29 @@ class MeshViewer:
             i = F[:,0] if F is not None else None,
             j = F[:,1] if F is not None else None,
             k = F[:,2] if F is not None else None,
-            showscale=True,
+            showscale=False,
         )
         self.data.append(mesh)
-    
+
+    # set_layout to view the correct T-pose
     def show(self, clear=True, **kwargs):
-        fig = go.Figure(data=self.data)
+        layout = go.Layout(
+            margin=dict(l=20, r=20, t=20, b=20),
+            width=400,
+            height=400,
+            scene = dict(
+                xaxis = dict(title='x'),
+                yaxis = dict(title='y'),
+                zaxis = dict(title='z'),
+            aspectmode='data', # preserve proportion of axis range
+            camera= dict(
+            up=dict(x=0, y=0, z=1), # z axis up
+            # up=dict(x=0, y=1, z=0), # y axis up
+            center=dict(x=0, y=0, z=0),
+            eye=dict(x=2, y=2, z=2) # zoom in (<1) or out (>1)
+        ))
+        )
+        fig = go.Figure(data=self.data, layout=layout)
         fig.show()
         if clear:
             self.data = []
